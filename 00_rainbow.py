9697026cf1711fb41d7d1b4065fc3faf2c3e3446
Kalie Tong <ktong@e1z2r3p48.42.us.org>
2019-12-07 15:41:54 -0800

    my head hurts

---
+++ 00_rainbow.py
@@ -0,0 +1,21 @@
+
+def print_colorful_text():
+    string.count('rainbow')
+    count=0
+    while len > count:
+        for each chr() in string:
+            count=count+1
+            if count = 1
+            text is blue
+    for style in range(8):
+        text.configure(font=(Times New Roman, 12, "bold"))
+        for foreground_color in range (30,38):
+            sl = ''
+            for background_color in range (40,48):
+                sl += '\x1b[%sm %s \x1b[0m' % (format, rainbow)
+            print(sl)
+        print('\n')
+
+print_colorful_text()
+
+
